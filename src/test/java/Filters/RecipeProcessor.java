9c9eaf535db2f51fd977ab0b44fcb7ae0d1829d0
Narmadha <Narmugitha@gmail.com>
2025-09-21 13:40:36 -0400

    Committing RecipeProcessor

--- src/test/java/Filters/RecipeProcessor.java
+++ src/test/java/Filters/RecipeProcessor.java
@@ -1,5 +1,45 @@
 package Filters;
 
-public class RecipeProcessor {
+import java.util.List;
 
+import RecipeData.RecipeUrlInfo;
+import Utilities.DataBaseConnection;
+import Utilities.ExcelReader;
+import Utilities.LoggerLoad;
+
+public class RecipeProcessor {
+    private final ExcelReader lchfFilter;
+    private final ExcelReader lfvFilter;
+    private final ExcelReader allergyFilter;
+
+    public RecipeProcessor(String excelPath) {
+        LoggerLoad.info("Initializing filters from Excel: " + excelPath);
+        this.lchfFilter = new ExcelReader(excelPath, 0, 0, 1);   // LCHF Sheet
+        this.lfvFilter  = new ExcelReader(excelPath, 1, 0, 1);   // LFV Sheet
+        this.allergyFilter = new ExcelReader(excelPath, 2, 0, 1); // Allergy Sheet
+    }
+
+    public void processRecipe(RecipeUrlInfo recipe) {
+        String ingredients = recipe.getIngredients();
+
+        List<String> lchfElim = lchfFilter.getMatchedEliminate(ingredients);
+        List<String> lchfAdd  = lchfFilter.getMatchedAdd(ingredients);
+
+        List<String> lfvElim = lfvFilter.getMatchedEliminate(ingredients);
+        List<String> lfvAdd  = lfvFilter.getMatchedAdd(ingredients);
+
+        List<String> allergyElim = allergyFilter.getMatchedEliminate(ingredients);
+        List<String> allergyAdd  = allergyFilter.getMatchedAdd(ingredients);
+
+        recipe.setLchfEliminate(String.join(", ", lchfElim));
+        recipe.setLchfToAdd(String.join(", ", lchfAdd));
+        recipe.setLfvEliminate(String.join(", ", lfvElim));
+        recipe.setLfvToAdd(String.join(", ", lfvAdd));
+        recipe.setAllergyEliminate(String.join(", ", allergyElim));
+        recipe.setAllergyToAdd(String.join(", ", allergyAdd));
+
+        LoggerLoad.debug("Recipe filtered: " + recipe.getRecipeName());
+
+        DataBaseConnection.insertRecipeData(recipe);
+    }
 }
